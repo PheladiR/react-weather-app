3a2ae04ec552ab12e7fe32f0a3340ba6fa67ef16
PheladiR <olgarachoshi@gmail.com>
2025-12-01 16:37:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     <p>Weather App</p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/PheladiR">Pheladi Rachoshi</a> and is{" "}
+        <a href="https://github.com/PheladiR/react-weather-app">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 };
