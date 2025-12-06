3f73dcd9e883d49be38477b29f2273920ded5175
PheladiR <olgarachoshi@gmail.com>
2025-12-06 15:02:30 +0200

    html structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/PheladiR">Pheladi Rachoshi</a> and is{" "}
-        <a href="https://github.com/PheladiR/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container border rounded p-4 mt-4">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/PheladiR"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pheladi Rachoshi
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/PheladiR/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default function Weather() {
+  return (
+    <div className="Weather container">
+      <div className="row">
+        <div className="col">
+          <h1>☀️27°C</h1>
+          <ul>
+            <li>Precipitation: 19%</li>
+            <li>Humidity: 40%</li>
+            <li>Wind: 10km/h</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h2>Polokwane</h2>
+          <ul>
+            <li>Saturday 10:00</li>
+            <li>Sunny</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3>Sat</h3>
+          <ul>
+            <li>☀️27°C</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Sun</h3>
+          <ul>
+            <li>☀️27°C</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Mon</h3>
+          <ul>
+            <li>☀️27°C</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Tue</h3>
+          <ul>
+            <li>☀️27°C</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Wed</h3>
+          <ul>
+            <li>☀️27°C</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
